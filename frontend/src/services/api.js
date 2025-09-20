a79358827c655124568153baaa8174c74b2fe7d4
soulemaneyeo99 <soulemaneyeo99@gmail.com>
2025-09-20 18:46:55 +0000

    config

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://opportunici-backend.onrender.com/api'; // Remplacez par l'URL de votre API
 
 // ---------------------- Axios Instance ----------------------
 
